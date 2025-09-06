9b3a08ab8790ba0ef6188d365c983d9b2d27353b
Coders Club <hi@codersclub.co>
2025-09-06 13:35:38 +0300

    Initial Commit

---
+++ MasterListApp/MasterListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterListAppApp.swift
+//  MasterListApp
+//
+//  Created by Mehmet Ali Sevdinoğlu on 6.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MasterListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
