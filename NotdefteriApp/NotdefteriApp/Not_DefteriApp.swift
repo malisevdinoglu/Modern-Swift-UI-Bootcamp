cf768fec58beca5a7f7da08cef4a2a65889575da
Coders Club <hi@codersclub.co>
2025-09-17 16:56:06 +0300

    Keep both projects side-by-side: NotdefteriApp and NotDefteri-CRUD

---
+++ NotdefteriApp/NotdefteriApp/Not_DefteriApp.swift
@@ -0,0 +1,17 @@
+//
+//  Not_DefteriApp.swift
+//  Not Defteri
+//
+//  Created by Mehmet Ali Sevdinoğlu on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotDefteriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
